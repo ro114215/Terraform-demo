1f1b8953c2853141fcf1c33154e815de87d0f8a1
Ross Uritz <ross.uritz@fmolhs.org>
2024-01-11 09:32:57 -0600

    a

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "terraform-state-bucket-637423327438-us-east-1" # bucket name
-#     key            = "terraform-demo-fmol/demo.tfstate" # path for the state file
-#     region         = "us-east-1" # region
-#     dynamodb_table = "terraform-locks-637423327438" #dynamodb table name
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-637423327438-us-east-1" # bucket name
+    key            = "terraform-demo-fmol/demo.tfstate" # path for the state file
+    region         = "us-east-1" # region
+    dynamodb_table = "terraform-locks-637423327438" #dynamodb table name
+    encrypt        = true
+  }
+}
